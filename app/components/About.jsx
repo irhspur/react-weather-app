eec82acdfbba2a6576e136007074199c66663a7a
irhspur <bajracharya.roop@gmail.com>
2018-04-24 08:42:46 +0545

    add grid system and style examples

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,7 +10,19 @@ var React = require('react');
 
 var About = (props) => {
     return(
-        <h3>About Component</h3>
+        <div>
+        <h1 className="text-center">About</h1>
+        <p>This is a weather application build on React.</p>
+        <p>Tools used:</p>
+        <ul>
+            <li>
+                <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+            </li>
+            <li>
+                <a href="http://openweathermap.org">Open Weather Map</a> - Used this weather map API.
+            </li>
+        </ul>
+        </div>
     )
 };
 
