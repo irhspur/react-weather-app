9fd72976e83105a7ffe0a95adb22201aa3a25512
irhspur <bajracharya.roop@gmail.com>
2018-04-26 08:20:40 +0545

    Style added

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -50,14 +50,14 @@ var Weather = React.createClass({
         function renderError() {
             if (typeof errorMessage === 'string') {
                 return (
-                    <ErrorModal/>
+                    <ErrorModal message={errorMessage}/>
                 )
             }
         }
 
         return(
             <div>
-                <h1 className="text-center">Get Weather</h1>
+                <h1 className="text-center page-title">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch}/>
                 {/* <WeatherMessage location={location} temp={temp}/> */}
                 {renderMessage()}
