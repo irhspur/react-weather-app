2ea416f095b60a610b042bf67b2ecd8f86a3a3a1
irhspur <bajracharya.roop@gmail.com>
2018-04-26 08:56:07 +0545

    add nav search component

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -19,8 +19,8 @@ var ErrorModal = React.createClass({
 
         return (
             <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-                <h4>{title}}</h4>
-                <p>{message}}</p>
+                <h4>{title}</h4>
+                <p>{message}</p>
                 <p>
                     <button className="button hollow" data-close="">
                         Okay
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -19,7 +19,9 @@ var Weather = React.createClass({
 
         this.setState({
             isLoading: true,
-            errorMessage: undefined
+            errorMessage: undefined,
+            location: undefined,
+            temp: undefined
         });
 
         openWeatherMap.getTemp(location).then(function (temp) {
@@ -35,6 +37,22 @@ var Weather = React.createClass({
             });
             alert(errorMessage);
         });
+    },
+    componentDidMount: function () {
+        var location = this.props.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
+    },
+    componentWillReceiveProps: function (newProps) {
+        var location = newProps.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
     },
     render: function () {
         var {isLoading, temp, location, errorMessage} = this.state;
